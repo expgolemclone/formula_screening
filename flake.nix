dad60f422f93eda42ccc93de47de8e8f096b1ec8
exp <nakanakananakada@gmail.com>
2026-04-04 15:23:09 +0900

    NixのdevShellでlibstdc++/zlibをLD_LIBRARY_PATHに追加

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "formula-screening dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.uv
+        ];
+
+        # pip/uvでインストールしたnumpy等のC拡張に必要なシステムライブラリを提供
+        env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc.lib # libstdc++.so.6
+          pkgs.zlib # libz.so.1
+        ];
+
+        shellHook = ''
+          # uvはPATH上のPythonではなく.venv内のPythonを使う
+          export UV_PROJECT_ENVIRONMENT="$PWD/.venv"
+        '';
+      };
+    };
+}
